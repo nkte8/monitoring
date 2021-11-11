a683d9c9dd0459f90aeff88dd4ab7141a6d17db7
nkte8 <nkhnd@outlook.com>
2021-11-11 22:39:57 +0900

    [add] hlsをmpegに変換するアプリケーションの作成

---
+++ hls2mpeg/src/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+owner=`ls -ld | awk '{ print $3 }'`
+useradd -M -u ${owner} rstpusr
+
+except=`date +'%Y-%m-%d'`
+
+target_paths=$(find ${PWD} -mindepth 2 -maxdepth 2 -not -name "${except}" -type d | grep -e "[0-9]\{4\}-[0-9]\{1,2\}-[0-9]\{1,2\}" -)
+
+for dir in ${target_paths}; do
+    LOCK_FILE="$(dirname ${dir})/.$(basename ${dir}).lock"
+    sleep $(($RANDOM % 10 * 10))
+    [[ -e ${LOCK_FILE} ]] && continue
+    TARGET_PATH=${dir}
+    break
+done
+
+echo "TARGET_PATH=${TARGET_PATH}"
+if [[ $TARGET_PATH = "" ]];then
+    echo "> no file to convert:)"
+    exit 0
+fi
+touch ${LOCK_FILE}.lock
+
+echo "> get file list..."
+filelist=$(find ${TARGET_PATH} -type f -name '*.ts')
+for f in ${filelist};do
+    echo "file '$f'" >> /tmp/mylist.txt
+done
+
+su rstpusr -c "ffmpeg -f concat -safe 0 -i /tmp/mylist.txt -vcodec copy -an ${TARGET_PATH}.mp4"
+rc=$?; [[ $rc -ne 0 ]] && exit $rc
+echo "> removing source video..."
+rm -rf "${TARGET_PATH}"
+rm -f "${LOCK_FILE}"
+exit 0
