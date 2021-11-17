7d486a2e023fef16701a265b976a7226d39af7d2
nkte8 <nkhnd@outlook.com>
2021-11-18 00:41:44 +0900

    [change] 排他処理の一部を変更

--- rtsp2hls/src/entrypoint.sh
+++ rtsp2hls/src/entrypoint.sh
@@ -7,16 +7,16 @@ if [[ -e "/app/config.csv" ]];then
     for row in `cat /app/config.csv`;do
         dev_name=`echo ${row} | cut -d , -f 1`
         sleep $(($RANDOM % 60)) 
-        if [[ $(find . -maxdepth 1 -name ".${dev_name}.lock") != "" ]];then 
-            echo "lockfile found. skip ${dev_name}..."
+        if [[ $(find . -maxdepth 1 -name "${dev_name}.m3u8") != "" ]];then 
+            echo "already hls playlist found. skip ${dev_name}..."
             continue
         fi
-        su rstpusr -c "touch .${dev_name}.lock"
+        su rstpusr -c "touch ${dev_name}.m3u8"
         python3 /app/printfps.py ${dev_name} > /app/fps
         rc=$?
         if [[ $rc -ne 0 ]];then
             echo "${dev_name}: Video stream seems not active."
-            rm -vf .${dev_name}.lock
+            rm -vf ${dev_name}.m3u8
             continue
         fi
 
@@ -32,14 +32,10 @@ if [[ $DEV_NAME = "" ]];then
 fi
 
 source /app/fps
-counter=0
-trap "rm -vf .${DEV_NAME}.lock" 1 2 3 15 EXIT
+trap "rm -vf ${DEV_NAME}.m3u8" 1 2 3 15 EXIT
 
 echo "device name: ${DEV_NAME}, segment fps: ${SEG_FPS}, segment time: ${SEG_TIME}, frames rotation: ${FRAME_ROTATE:=Rotate_0}"
-while [[ $counter -le 3 ]];do
-    su rstpusr -c "python3 /app/rtsp2frame.py ${DEV_NAME} ${FRAME_ROTATE} | ffmpeg -r ${SEG_FPS} -i - -c:v libx264 -strftime 1 -strftime_mkdir 1 -hls_segment_filename ${DEV_NAME}/%Y-%m-%d/v%H%M%S.ts -sc_threshold 0 -g ${SEG_FPS} -keyint_min $(awk "BEGIN { print $SEG_FPS * $SEG_TIME }") -hls_time ${SEG_TIME} ${DEV_NAME}.m3u8"
-    rc=$?
-    counter=$((counter + 1))
-    echo "> ffmpeg process finished[$counter]: rc=$rc"
-done
+su rstpusr -c "python3 /app/rtsp2frame.py ${DEV_NAME} ${FRAME_ROTATE} | ffmpeg -r ${SEG_FPS} -i - -c:v libx264 -strftime 1 -strftime_mkdir 1 -hls_segment_filename ${DEV_NAME}/%Y-%m-%d/v%H%M%S.ts -sc_threshold 0 -g ${SEG_FPS} -keyint_min $(awk "BEGIN { print $SEG_FPS * $SEG_TIME }") -hls_time ${SEG_TIME} ${DEV_NAME}.m3u8"
+rc=$?
+echo "> ffmpeg process finished: rc=$rc"
 exit $rc
--- rtsp2hls/src/sigterm.sh
+++ rtsp2hls/src/sigterm.sh
@@ -2,4 +2,4 @@
 echo "> get process id"
 ID=$(ps -aux | grep "su rstpusr -c" | awk '{ print $2 }')
 
-kill $ID && kill -15 1
+kill -9 $ID && kill -15 1
