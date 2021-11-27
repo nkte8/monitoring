24a12ce3c0af5f0bdf5c4726ee6ecc813b90bac6
nkte8 <nkhnd@outlook.com>
2021-11-28 00:50:11 +0900

    [change] config.csvの4列目にIPアドレス指定できるよう変更

--- rtsp2hls/src/entrypoint.sh
+++ rtsp2hls/src/entrypoint.sh
@@ -6,13 +6,15 @@ if [[ -e "/app/config.csv" ]];then
     echo "read /app/config.csv..." 
     for row in `cat /app/config.csv`;do
         dev_name=`echo ${row} | cut -d , -f 1`
+        ip_addr=`echo ${row} | cut -d , -f 4`
+        [[ $ip_addr = "" ]] && ip_addr=${dev_name}
         sleep $(($RANDOM % 60)) 
         if [[ $(find . -maxdepth 1 -name "${dev_name}.m3u8") != "" ]];then 
             echo "already hls playlist found. skip ${dev_name}..."
             continue
         fi
         su rstpusr -c "touch ${dev_name}.m3u8"
-        python3 /app/printfps.py ${dev_name} > /app/fps
+        python3 /app/printfps.py ${ip_addr} > /app/fps
         rc=$?
         if [[ $rc -ne 0 ]];then
             echo "${dev_name}: Video stream seems not active."
@@ -21,6 +23,7 @@ if [[ -e "/app/config.csv" ]];then
         fi
 
         DEV_NAME=${dev_name}
+        IP_ADDR=${ip_addr}
         SEG_TIME=`echo ${row} | cut -d , -f 2`
         FRAME_ROTATE=`echo ${row} | cut -d , -f 3`
         break
@@ -34,8 +37,8 @@ fi
 source /app/fps
 trap "rm -vf ${DEV_NAME}.m3u8" 1 2 3 15 EXIT
 
-echo "device name: ${DEV_NAME}, segment fps: ${SEG_FPS}, segment time: ${SEG_TIME}, frames rotation: ${FRAME_ROTATE:=Rotate_0}"
-su rstpusr -c "python3 /app/rtsp2frame.py ${DEV_NAME} ${FRAME_ROTATE} | ffmpeg -r ${SEG_FPS} -i - -c:v libx264 -strftime 1 -strftime_mkdir 1 -hls_segment_filename ${DEV_NAME}/%Y-%m-%d/v%H%M%S.ts -sc_threshold 0 -g ${SEG_FPS} -keyint_min $(awk "BEGIN { print $SEG_FPS * $SEG_TIME }") -hls_time ${SEG_TIME} ${DEV_NAME}.m3u8"
+echo "device name: ${DEV_NAME},ip address(or DNS): ${IP_ADDR}, segment fps: ${SEG_FPS}, segment time: ${SEG_TIME}, frames rotation: ${FRAME_ROTATE:=Rotate_0}"
+su rstpusr -c "python3 /app/rtsp2frame.py ${IP_ADDR} ${FRAME_ROTATE} | ffmpeg -r ${SEG_FPS} -i - -c:v libx264 -strftime 1 -strftime_mkdir 1 -hls_segment_filename ${DEV_NAME}/%Y-%m-%d/v%H%M%S.ts -sc_threshold 0 -g ${SEG_FPS} -keyint_min $(awk "BEGIN { print $SEG_FPS * $SEG_TIME }") -hls_time ${SEG_TIME} ${DEV_NAME}.m3u8"
 rc=$?
 echo "> ffmpeg process finished: rc=$rc"
 exit $rc
